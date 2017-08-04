2e6e3c3ca4df8157db068aacca326d87ce86d9f8
Paulo Mellin Gimenes <pmellingimenes@gmail.com>
2017-08-04 20:48:21 -0300

    Criando o modelo Tarefa no Backend

--- backend/server/models.py
+++ backend/server/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Tarefa(models.Model):
+    title = models.CharField(max_length=64)
+    completed = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('created',)
